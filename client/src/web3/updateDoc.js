402daa54e427a4bc01c9f0b204745af1db56d8bd
SOMESH BANERJEE <54861461+somesh-banerjee@users.noreply.github.com>
2022-05-06 05:19:36 +0530

    Create update in web3

---
+++ client/src/web3/updateDoc.js
@@ -0,0 +1,46 @@
+import { ethers } from 'ethers'
+import Web3Modal from 'web3modal'
+import { create as ipfsHttpClient } from 'ipfs-http-client'
+
+import {
+    contractAddress
+} from '../config'
+
+const client = ipfsHttpClient('https://ipfs.infura.io:5001/api/v0')
+
+import ABI from "./Editor.json"
+
+export default function UpdateDoc(key,file) {
+
+    let url
+    try {
+        const added = await client.add(
+            file,
+            {
+                progress: (prog) => console.log(`received: ${prog}`)
+            }
+        )
+        //creating ipfs url with the hash
+        url = `https://ipfs.infura.io/ipfs/${added.path}`
+    } catch (error) {
+        console.log(error);
+    }
+
+    const web3Modal = new Web3Modal()
+    const connection = await web3Modal.connect()
+    const provider = new ethers.providers.Web3Provider(connection)
+    const signer = provider.getSigner()
+
+    const Contract = new ethers.Contract(contractAddress, ABI.abi, signer)
+    
+    try {
+        let transaction = await Contract.newVersion(key,url)
+        console.log(transaction);
+        let tx = await transaction.wait()
+        let event = tx.events[0]
+        console.log(event);
+    } catch (err) {
+        console.log(err);
+    }
+
+}
