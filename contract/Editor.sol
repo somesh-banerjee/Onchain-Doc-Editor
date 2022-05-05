8d827924bafa2723e09b3e435aa57cacd682c1bc
SOMESH BANERJEE <54861461+somesh-banerjee@users.noreply.github.com>
2022-05-06 04:52:04 +0530

    Create Editor.sol
    Create Contract

---
+++ contract/Editor.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.7;
+
+contract Editor{
+    struct Document{
+        address owner;
+        uint256[] time;
+        string[] versions; 
+    }
+    mapping(string => Document) public docs;
+
+    function newDoc(string memory _key, string memory _url) public {
+        docs[_key].owner = msg.sender;
+        docs[_key].versions.push(_url);
+        docs[_key].time.push(block.timestamp);
+    }
+
+    function newVersion(string memory _key, string memory _url) public {
+        require(msg.sender == docs[_key].owner,"Only owner can update");
+        docs[_key].versions.push(_url);
+        docs[_key].time.push(block.timestamp);
+    }
+
+    function getAllVersions(string memory _key) public view returns(string[] memory){
+        return docs[_key].versions;
+    }
+}
