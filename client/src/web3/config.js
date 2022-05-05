9246d4262218850685ca5bcb7ca2a4364cd8c167
SOMESH BANERJEE <54861461+somesh-banerjee@users.noreply.github.com>
2022-05-06 05:11:12 +0530

    Create ne Document web3

---
+++ client/src/web3/config.js
@@ -0,0 +1,7 @@
+//In Mumbai Testnet
+export const contractAddress = ""
+
+/*
+//In local
+export const contractAddress = ""
+*/
---
+++ client/src/web3/createDoc.js
@@ -0,0 +1,30 @@
+import { ethers } from 'ethers'
+import Web3Modal from 'web3modal'
+
+import {
+    contractAddress
+} from '../config'
+
+
+import ABI from "./Editor.json"
+
+export default function CreateDoc(key) {
+
+    const web3Modal = new Web3Modal()
+    const connection = await web3Modal.connect()
+    const provider = new ethers.providers.Web3Provider(connection)
+    const signer = provider.getSigner()
+
+    const Contract = new ethers.Contract(contractAddress, ABI.abi, signer)
+    
+    try {
+        let transaction = await Contract.newDoc(key)
+        console.log(transaction);
+        let tx = await transaction.wait()
+        let event = tx.events[0]
+        console.log(event);
+    } catch (err) {
+        console.log(err);
+    }
+
+}
